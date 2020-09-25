6f51f7dec0ae0f8a42acd134f0ed3b10dc151416
firaskahlout <firaskahlout@outlook.com>
2020-09-25 20:08:06 +0300

    add project files

---
+++ MyAnimeList/Application/MyAnimeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAnimeListApp.swift
+//  MyAnimeList
+//
+//  Created by Firas AlKahlout on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct MyAnimeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TopAnimeView()
+        }
+    }
+}
---
+++ MyAnimeList/Flows/TopAnimeList/TopAnimeView.swift
@@ -0,0 +1,20 @@
+//
+//  TopAnimeView.swift
+//  MyAnimeList
+//
+//  Created by Firas AlKahlout on 9/23/20.
+//
+
+import SwiftUI
+
+struct TopAnimeView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopAnimeView()
+    }
+}
